8aa0051817a15fce68ef4e66d912ae0f4055e33d
Sir-Ross <43891611+Sir-Ross@users.noreply.github.com>
2023-04-11 10:52:01 -0500

    Add files via upload

--- adjacency_to_link.py
+++ adjacency_to_link.py
@@ -1,10 +1,14 @@
 f_in = open("adjacency_list_old.txt", "r")
 f_out = open("link.txt", "w")
+f_out2 = open("isolates.txt", "w")
 lines = f_in.readlines()
 for line in lines:
     names = line.split('|')
-    for i in range(1,len(names)):
-        # Run for each name except first name
-        f_out.write('["'+names[0].replace('\n','')+'", "'+names[i].replace('\n','')+'"],\n')
+    if len(names) == 1:
+        f_out2.write('"'+names[0]+'",')
+    else:
+        for i in range(1,len(names)):
+            # Run for each name except first name
+            f_out.write('["'+names[0].replace('\n','')+'", "'+names[i].replace('\n','')+'"],\n')
 f_in.close()
 f_out.close()
